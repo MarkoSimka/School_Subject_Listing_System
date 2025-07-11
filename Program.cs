ba1145535205ceddbfdfbbc20da3d22c9c3f8bc6
MarkoSimka <63017129+MarkoSimka@users.noreply.github.com>
2025-07-11 14:07:15 +0200

    chore: initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace School_Subjects_Listing_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
