2d7f9c9c5b745adeb412059270e5025e7262c874
MarkoSimka <63017129+MarkoSimka@users.noreply.github.com>
2025-07-11 16:54:33 +0200

    feat(config): add startup and database configuration

---
+++ Config/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Damilah_School_Subject_App.Repository.Implementation;
+using Damilah_School_Subject_App.Repository.Interface;
+using Damilah_School_Subject_App.Service.Implementation;
+using Damilah_School_Subject_App.Service.Interface;
+using Microsoft.Extensions.DependencyInjection;
+using static Damilah_School_Subject_App.Config.DbConstants;
+
+namespace Damilah_School_Subject_App.Config
+{
+    public class StartupServiceProvider
+    {
+        private DbConfig DbConfig { get; set; }
+
+        public SubjectRepository SetupSubjectRepository(IServiceProvider serviceProvider)
+        {
+            try
+            {
+                DbConfig = serviceProvider.GetRequiredService<DbConfig>();
+                return new SubjectRepository(DbConfig.GetConnectionString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
+
+        }
+
+        public ServiceProvider GetServices()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton(new DbConfig())
+                .AddSingleton<ISubjectRepository>(provider => SetupSubjectRepository(provider))
+                .AddSingleton<ISubjectService, SubjectService>()
+                .AddSingleton<IExternalApiHandlerService, ExternalApiHandlerService>()
+                .BuildServiceProvider();
+
+
+            return serviceProvider;
+        }
+
+    }
+}
